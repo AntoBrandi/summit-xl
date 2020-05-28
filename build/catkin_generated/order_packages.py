4225dae8a566e9a862d1fce37569b45052c1f4f7
AntoBrandi <antonio.brandi@outlook.it>
2020-05-28 16:40:53 +0200

    first

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/legion/summit-xl/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/legion/summit-xl/devel;/opt/ros/melodic'.split(';') if '/home/legion/summit-xl/devel;/opt/ros/melodic' != '' else []
